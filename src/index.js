362453b2d8f93dbee0c06a5fd86f525004eaf0c3
Alena Lobacheva <danarimer785@gmail.com>
2024-05-14 15:40:43 +0400

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import readlineSync from "readline-sync";
+const roundCount = 3;
+
+export default (description, getQuestionAnswer) => {
+  console.log("Welcome to the Brain Games!");
+  const name = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${name}!`);
+
+  console.log(description);
+
+  for (let i = 0; i < roundCount; i++) {
+    const [question, correctAnswer] = getQuestionAnswer();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question("Your answer: ");
+    if (answer === correctAnswer) {
+      console.log("Correct!");
+    } else {
+      console.log(
+        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}`
+      );
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
