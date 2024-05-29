97cbadb2fe49a6a579c9cc71d3c6daf4afa78477
Alena Lobacheva <danarimer785@gmail.com>
2024-05-29 16:49:38 +0400

    upd config

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,18 +2,23 @@ import game from "../index.js";
 
 import { getRandomNumber } from "../index.js";
 
-const description = 'What is the result of the expression?';
+const description = "What is the result of the expression?";
 const getRandomOperator = () => {
-    const operators = ['+', '-', '*'];
-    const randomIndex = Math.floor(Math.random() * operators.length);
-    return operators[randomIndex];
-  };
-  const getQuestionAnswer = () => {
-  const question = getRandomNumber(1, 100) + ' ' + getRandomOperator() + ' ' + getRandomNumber(1, 100);
+  const operators = ["+", "-", "*"];
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  return operators[randomIndex];
+};
+const getQuestionAnswer = () => {
+  const question =
+    getRandomNumber(1, 100) +
+    " " +
+    getRandomOperator() +
+    " " +
+    getRandomNumber(1, 100);
   const correctAnswer = eval(question).toString();
   return [question, correctAnswer];
-  };
-  
-  export default () => {
-    game(description, getQuestionAnswer);
-  };
+};
+
+export default () => {
+  game(description, getQuestionAnswer);
+};
