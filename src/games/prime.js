5b70140d24f18d34ef37b054f466ac120e6a95ce
Alena Lobacheva <danarimer785@gmail.com>
2024-06-04 17:10:45 +0400

    add new game prime

---
+++ src/games/prime.js
@@ -0,0 +1,27 @@
+import game from "../index.js";
+
+import { getRandomNumber } from "../index.js";
+
+const description =
+  'Answer "yes" if given number is prime. Otherwise answer "no"';
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getQuestionAnswer = () => {
+  const question = getRandomNumber(1, 20);
+  const correctAnswer = isPrime(question) ? "yes" : "no";
+  return [question, correctAnswer];
+};
+
+export default () => {
+  game(description, getQuestionAnswer);
+};
