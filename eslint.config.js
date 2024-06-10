1b3d1b7a5adc13d2dde2ce709e0e975bfeefe088
Alena Lobacheva <danarimer785@gmail.com>
2024-06-10 15:55:42 +0400

    upd eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import airbnbBase from "eslint-config-airbnb-base";
+import airbnbBaseConfig from "eslint-config-airbnb-base";
 
 export default [
-  {
-    languageOptions: {
-      globals: globals.browser,
-    },
-    rules: {
-      ...airbnbBase.rules,
-    },
-  },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      ...airbnbBaseConfig.rules 
+    }
+  }
 ];
+
